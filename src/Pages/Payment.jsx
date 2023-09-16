1ecf74ace04fc0d1a761aa77638c89a093a10e61
SutharShantanu <shantanusut2000@gmail.com>
2023-09-17 01:14:26 +0530

    Key is fetching in Front-end

--- src/Pages/Payment.jsx
+++ src/Pages/Payment.jsx
@@ -1,50 +1,64 @@
 import React from "react";
 import PayCard from "../Components/PayCard";
 import axios from "axios";
-import Razorpay from "razorpay";
 
 const Payment = () => {
     const handlePayment = async (amount) => {
-        // console.log(window.Razorpay);
+        try {
+            const KeyData = await axios.get(`http://localhost:4500/pay/getkey`);
+            console.log(KeyData.data.key);
+            // const key = KeyData.data.key;
+            // key = JSON.stringify(key);
 
-        // const {
-        //     data: { key },
-        // } = await axios.get("http://www.localhost:4500/pay/getkey");
+            const OrderData = await axios.post(
+                `http://localhost:4500/pay/checkout`,
+                { amount }
+            );
 
-        // const {
-        //     data: { order },
-        // } = await axios.post("http://localhost:4500/pay/checkout", {
-        //     amount,
-        // });
+            console.log(OrderData);
 
-        // const options = {
-        //     key,
-        //     amount: order.amount,
-        //     currency: "INR",
-        //     name: "Testing Payment",
-        //     description: "RazorPay Payment Integration",
-        //     image: "https://dummyimage.com/460x460/e371e3/fff.png",
-        //     order_id: order.id,
-        //     callback_url: "http://localhost:4500/pay/paymentverification",
-        //     prefill: {
-        //         name: "User Name",
-        //         email: "user.email@example.com",
-        //         contact: "9999999999",
-        //     },
-        //     notes: {
-        //         address: "Razorpay Corporate Office",
-        //     },
-        //     theme: {
-        //         color: "#121212",
-        //     },
-        // };
-        // const razor = new window.Razorpay(options);
-        // razor.open();
+            // const order = OrderData.data;
+            // console.log(order);
+            // order = JSON.stringify(order);
+
+            // const {
+            //     data: { order },
+            // } = await axios.post("http://localhost:4500/pay/checkout", {
+            //     amount,
+            // });
+            // console.log(order, key);
+
+            // const options = {
+            //     key,
+            //     amount: order.amount,
+            //     currency: "INR",
+            //     name: "6 Pack Programmer",
+            //     description: "Tutorial of RazorPay",
+            //     image: "https://avatars.githubusercontent.com/u/25058652?v=4",
+            //     order_id: order.id,
+            //     callback_url: "http://localhost:4500/pay/paymentverification",
+            //     prefill: {
+            //         name: "Gaurav Kumar",
+            //         email: "gaurav.kumar@example.com",
+            //         contact: "9999999999",
+            //     },
+            //     notes: {
+            //         address: "Razorpay Corporate Office",
+            //     },
+            //     theme: {
+            //         color: "#121212",
+            //     },
+            // };
+            // const razor = new window.Razorpay(options);
+            // razor.open();
+        } catch (error) {
+            console.log(error.message);
+        }
     };
     return (
         <div>
             <PayCard
-                amount={500}
+                amount={5000}
                 img={`https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80`}
                 handlePayment={handlePayment}
             />
