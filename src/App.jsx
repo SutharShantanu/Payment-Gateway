519094b7e72adf7fe66d1af9ecb56bade6a0ed16
Shantanu <shantanusut2000@gmail.com>
2023-09-16 21:38:01 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import PaymentCard from "./Components/PaymentCard";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <PaymentCard />
+    </div>
+  );
+}
